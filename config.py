afa565535e5680e5fced0fd2b9d7a8cff2d6caa1
Nestor Villa <nestorvillap@gmail.com>
2024-10-30 13:48:40 +0000

    Configuracion cambiada

--- config.py
+++ config.py
@@ -5,8 +5,14 @@ import os
 load_dotenv()
 
 # Extraer las variables necesarias
+#Database
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_USERNAME = os.getenv("DB_USERNAME")
 URL_SERVER = os.getenv("URL_SERVER")
 DB_NAME = os.getenv("DB_NAME")
 URL_DB = f"mongodb+srv://{DB_USERNAME}:{DB_PASSWORD}{URL_SERVER}"
+#Cache
+CACHE_HOST = os.getenv("CACHE_HOST")
+CACHE_PORT = int(os.getenv("CACHE_PORT"))
+CACHE_USERNAME = os.getenv("CACHE_USERNAME")
+CACHE_PASSWORD = os.getenv("CACHE_PASSWORD")
